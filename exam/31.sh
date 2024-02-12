76c7d10e2401a2cf2dd7fa117f8f7af54ff57afb
Lorenzo Calandra Buonaura <lorenzocb01@gmail.com>
2024-02-12 20:32:13 +0100

    Ex3.1 -> good

---
+++ exam/31.sh
@@ -0,0 +1,29 @@
+mkdir "./students"
+cd "./students"
+
+if [ ! -f ./LCP_22-23_students.csv ]
+then
+    wget https://www.dropbox.com/s/867rtx3az6e9gm8/LCP_22-23_students.csv
+fi
+
+grep "PoD" "./LCP_22-23_students.csv" > "./PoD_students.csv"
+grep "Physics" "./LCP_22-23_students.csv" > "./Physics_students.csv"
+
+max_count=0
+max_letter="A"
+
+for letter in {A..Z}
+do
+    count=$(awk -F '[, ]' 'NR>1 {print substr($1, 1, 1)}' "./LCP_22-23_students.csv" | grep -c "$letter")
+    echo "$letter: $count" >> "./count_by_letters.csv"
+
+    if [ $count -gt $max_count ]
+    then
+        max_count=$count
+        max_letter=$letter
+    fi
+done
+
+echo "The most frequent surname initial is: $max_letter."
+
+awk -F ',' 'NR>1 {file="./group_"int((NR-2)%18)+1".txt"; print $0 >> file; close(file)}' ./LCP_22-23_students.csv
