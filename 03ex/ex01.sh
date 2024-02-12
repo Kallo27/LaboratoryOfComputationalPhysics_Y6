3375fa28a1ed11fedc6e7e92c797ce7dce4febe6
Lorenzo Calandra Buonaura <lorenzocb01@gmail.com>
2024-02-12 20:06:22 +0100

    Correct 3.1

--- 03ex/ex01.sh
+++ 03ex/ex01.sh
@@ -1,28 +1,29 @@
 mkdir "./students"
-
 cd "./students"
 
-if [ ! -f ./LCP_22-23_students.csv ] 
+if [ ! -f ./LCP_22-23_students.csv ]
 then
     wget https://www.dropbox.com/s/867rtx3az6e9gm8/LCP_22-23_students.csv
 fi
 
-grep "PoD" "./LCP_22-23_students.csv" > "./PoD_Students"
-grep "Physics" "./LCP_22-23_students.csv" > "./Physics_Students"
+grep "PoD" "./LCP_22-23_students.csv" > "./Pod_students"
+grep "Physics" "./LCP_22-23_students.csv" > "./Physics_students"
 
-max=0
+max_count=0
 max_letter="A"
 
 for letter in {A..Z}
 do
-    count=$(awk -F '[, ]' '{if(NR>1) print $1}' "./LCP_22-23_students.csv" | grep -c "$letter")
-    echo "$letter: $count" >> "./count_by_letter"
+    count=$(awk -F '[, ]' 'NR>1 {print substr($1, 1, 1)}' "./LCP_22-23_students.csv" | grep -c "$letter")
+    echo "$letter: $count" >> './count_by_letter'
 
-    if [ $count -gt $max ]
+    if [ $count -gt $max_count ]
     then
-        max=$count
+        max_count=$count
         max_letter=$letter
     fi
 done
 
-echo "The letter with the most appearances is: $max_letter"
+echo "The letter with most appearences is: $max_letter"
+
+awk -F',' 'NR>1 {file="./group_"int((NR-2)%18)+1".txt"; print $0 >> file; close(file)}' ./LCP_22-23_students.csv
