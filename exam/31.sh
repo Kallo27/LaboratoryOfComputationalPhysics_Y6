3ca97821a6cd53979d822566d15607dc951aa295
Lorenzo Calandra Buonaura <lorenzocb01@gmail.com>
2024-02-13 10:51:08 +0100

    Ex3 -> repeated

--- exam/31.sh
+++ exam/31.sh
@@ -6,16 +6,16 @@ then
     wget https://www.dropbox.com/s/867rtx3az6e9gm8/LCP_22-23_students.csv
 fi
 
-grep "PoD" "./LCP_22-23_students.csv" > "./PoD_students.csv"
-grep "Physics" "./LCP_22-23_students.csv" > "./Physics_students.csv"
+grep "PoD" "./LCP_22-23_students.csv" > "./PoD_students"
+grep "Physics" "./LCP_22-23_students.csv" > "./Physics_students"
 
 max_count=0
 max_letter="A"
 
 for letter in {A..Z}
 do
-    count=$(awk -F '[, ]' 'NR>1 {print substr($1, 1, 1)}' "./LCP_22-23_students.csv" | grep -c "$letter")
-    echo "$letter: $count" >> "./count_by_letters.csv"
+    count=$(awk -F '[, ]' 'NR>1 {print substr($1, 1, 1)}' ./LCP_22-23_students.csv | grep -c "$letter")
+    echo "$letter: $count" >> "./count_by_letter"
 
     if [ $count -gt $max_count ]
     then
@@ -24,6 +24,6 @@ do
     fi
 done
 
-echo "The most frequent surname initial is: $max_letter."
+echo "The most frequent initial is: $max_letter."
 
 awk -F ',' 'NR>1 {file="./group_"int((NR-2)%18)+1".txt"; print $0 >> file; close(file)}' ./LCP_22-23_students.csv
--- exam/32.sh
+++ exam/32.sh
@@ -3,12 +3,12 @@ grep -v '^#' data.csv | tr -d ',' > data.txt
 even_numbers_count=$(grep -oE '\b[0-9]+\b' data.txt | awk '$1 % 2 == 0' | wc -l)
 echo "Number of even numbers: $even_numbers_count."
 
-num_greater=0
 num_lower=0
+num_greater=0
 threshold=$(echo "scale=6; 100 * sqrt(3) / 2" | bc -l)
 
 while IFS=' ' read -r X Y Z Xp Yp Zp
-do 
+do
     distance=$(echo "scale=6; sqrt($X^2 + $Y^2 + $Z^2)" | bc -l)
     if [ $(echo "$distance > $threshold" | bc -l) -eq 1 ]
     then
@@ -19,19 +19,19 @@ do
 
 done < data.txt
 
-echo "Number of distances higher than the treshold: $num_greater"
-echo "Number of distances lower than the treshold: $num_lower"
+echo "Number of distances higher than the threshold: $num_greater."
+echo "Number of distances lower than the threshold: $num_lower."
 
 read -p "Enter the number of files (n):" n
 
 if [[ ! $n =~ ^[1-9][0-9]*$ ]]
 then
-    echo "Not valid input: n must be a positive integer"
+    echo "The input is not valid; n must be a positive integer."
     exit 1
 fi
 
-for (( i = 1; i <= n; i++ ))
-do 
+for (( i=1; i<=n; i++ ))
+do
     while IFS=' ' read -r X Y Z Xp Yp Zp
     do
         X=$(echo "scale=6; $X / $i" | bc -l)
@@ -41,9 +41,9 @@ do
         Yp=$(echo "scale=6; $Yp / $i" | bc -l)
         Zp=$(echo "scale=6; $Zp / $i" | bc -l)
 
-        echo "$X $Y $Z $Xp $Yp $Zp">> "./data_$i.txt"
+    echo "$X $Y $Z $Xp $Yp $Zp" >> "./data_$i.txt"
     done < "data.txt"
 
 done
 
-echo "Copies created successfully"
+echo "Copies created successfully."
